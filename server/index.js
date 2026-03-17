80298d0c9194557127918a5bd84981a877109e74
Karan Ankade <karanankade12@gmail.com>
2026-03-18 00:02:51 +0530

    fix: fallback port for render

--- server/index.js
+++ server/index.js
@@ -34,7 +34,8 @@ app.use((err, req, res, next) => {
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
+    const PORT = process.env.PORT || 5000;
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error("MongoDB connection error:", err));
